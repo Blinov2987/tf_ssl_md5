d9b7c08d5dab5bd0dac0d9f76c6554455f38bf05
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2021-02-06 16:19:51 +0300

    pre vaccine

--- includes/des.h
+++ includes/des.h
@@ -37,7 +37,7 @@ static const uint8_t	g_ip_out[] = {
 		33, 1, 41, 9, 49, 17, 57, 25
 };
 
-static const uint8_t	g_key_ext[] = {
+static const uint8_t	g_key_ext_in[] = {
 		57, 49, 41, 33, 25, 17, 9,
 		1, 58, 50, 42, 34, 26, 18,
 		10, 2, 59, 51, 43, 35, 27,
@@ -48,7 +48,7 @@ static const uint8_t	g_key_ext[] = {
 		21, 13, 5, 28, 20, 12, 4
 };
 
-static const uint8_t	g_key_round[] = {
+static const uint8_t	g_key_ext_out[] = {
 		14, 17, 11, 24, 1, 5,
 		3, 28, 15, 6, 21, 10,
 		23, 19, 12, 4, 26, 8,
@@ -59,7 +59,7 @@ static const uint8_t	g_key_round[] = {
 		46, 42, 50, 36, 29, 32
 };
 
-static const uint8_t	g_extend[] = {
+static const uint8_t	g_extend[48] = {
 		32, 1, 2, 3, 4, 5,
 		4, 5, 6, 7, 8, 9,
 		8, 9, 10, 11, 12, 13,
--- srcs/cipher_func/des_starter.c
+++ srcs/cipher_func/des_starter.c
@@ -6,7 +6,7 @@
 /*   By: gemerald <gemerald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:19:20 by gemerald          #+#    #+#             */
-/*   Updated: 2021/02/05 20:59:38 by gemerald         ###   ########.fr       */
+/*   Updated: 2021/02/06 16:15:02 by gemerald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,27 +29,33 @@ uint64_t perrm_64(uint64_t num, const uint8_t ip_tab[], size_t tab_len, size_t s
 	return (result);
 }
 
-uint32_t feistel(uint32_t part, uint64_t key)
+uint32_t f_expansion(uint32_t part, uint64_t key)
 {
-	uint32_t result;
+	uint64_t result;
 	uint64_t expanded;
 
-	expanded = perrm_64(part, g_extend, 48, 64);
+	expanded = perrm_64(part, g_extend, 48, 32);
+	result = expanded ^ key;
+	int i = 0;
+	return (result);
 }
 
-uint64_t  make_f_net(uint64_t num, uint64_t key)
+uint64_t  make_f_net(uint64_t num, uint64_t key[])
 {
 	int i;
 	uint64_t result;
 	uint32_t left;
 	uint32_t right;
+	uint32_t left_in;
+	uint32_t right_in;
 
 	i = -1;
-	left = (uint32_t)(num >> 32);
-	right = (uint32_t)num;
+	left_in = (uint32_t)(num >> 32);
+	right_in = (uint32_t)num;
 	while (++i < 16)
 	{
-
+		left = right_in;
+		right = left_in ^ f_expansion(right_in, key[i]);
 	}
 }
 
@@ -58,28 +64,29 @@ uint32_t left_shift_28b(uint32_t val, uint32_t shift)
 	return (val >> (28 - shift)) | ((val << shift) & 0xfffffff);
 }
 
-void 	key_gen(uint64_t keys[], uint64_t orig_key)
+void 	key_gen(uint64_t keys[16], uint64_t orig_key)
 {
 	int i;
 	uint32_t in_left;
 	uint32_t in_right;
-	uint32_t left[16];
-	uint32_t right[16];
+	uint32_t halfs[32];
 	uint64_t permut_key;
 
-	i = -1;
+	i = 0;
 	ft_bzero(keys, 8 * 16);
-	ft_bzero(left, 4 * 16);
-	ft_bzero(right, 4 * 16);
-	permut_key = perrm_64(orig_key, g_key_ext, 56, 64);
+	ft_bzero(halfs, 4 * 32);
+	permut_key = perrm_64(orig_key, g_key_ext_in, 56, 64);
 	in_left = permut_key >> 28;;
 	in_right = permut_key & 0xfffffff;
-	while (++i < 16)
+	while (i < 31)
 	{
-		left[i] = left_shift_28b(in_left, g_shift_left[i]);
-		right[i] = left_shift_28b(in_right, g_shift_left[i]);
-		in_left = left[i];
-		in_right = right[i];
+		halfs[i] = left_shift_28b(in_left, g_shift_left[i / 2]);
+		halfs[i + 1] = left_shift_28b(in_right, g_shift_left[i / 2]);
+		in_left = halfs[i];
+		in_right = halfs[i + 1];
+		keys[i / 2] = (((uint64_t)in_left) << 28) | (uint64_t)in_right;
+		keys[i / 2] = perrm_64(keys[i / 2], g_key_ext_out, 48, 56);
+		i += 2;
 	}
 }
 
@@ -97,6 +104,7 @@ void 	make_des_rounds(uint64_t *mem, size_t size, uint64_t key)
 	while (round < size)
 	{
 		current = perrm_64(mem[round], g_ip_in, 64, 64);
+		current = make_f_net(current, keys);
 		current = perrm_64(current, g_ip_out, 64, 64);
 		round++;
 	}
