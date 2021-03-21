61f8f81833d79de9edac55d1505aebfbdd58fe79
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2021-03-21 20:04:08 +0300

    add converters and checkers

--- srcs/rsa_utils/key_stream_reader.c
+++ srcs/rsa_utils/key_stream_reader.c
@@ -6,7 +6,7 @@
 /*   By: gemerald <gemerald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 16:41:19 by gemerald          #+#    #+#             */
-/*   Updated: 2021/03/21 16:41:19 by gemerald         ###   ########.fr       */
+/*   Updated: 2021/03/21 16:48:02 by gemerald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,58 @@ t_list  *read_der_form(int fd)
 	return (buffered_reader(fd));
 }
 
+size_t get_end_offset(uint8_t *mem, size_t size)
+{
+	size_t offset;
+
+	offset = 0;
+	while (--size > 0)
+	{
+
+		if (!ft_isspace(mem[size]))
+			break ;
+		offset++;
+	}
+	return (offset);
+}
+
+t_list *cut_start_end(t_list **pem, size_t diff, size_t off)
+{
+	uint8_t *new_mem;
+	size_t	new_size;
+
+	new_size = (*pem)->content_size - diff;
+	new_mem = (uint8_t *)ft_safe_memalloc(new_size, "cut_priv");
+	ft_mem_copy(new_mem, (&((uint8_t *)(*pem)->content)[off], new_size);
+	free((*pem)->content);
+	(*pem)->content = new_mem;
+	(*pem)->content_size = new_size;
+	return (*pem);
+}
+
 t_list *read_pem_form(int fd)
 {
 	t_list *pem;
 
 	pem = buffered_reader(fd);
+	if (pem->content_size < 50)
+	{
+		ft_lst_free(&pem);
+		return (NULL);
+	}
+	if ((ft_memcmp(pem->content, RSA_PRIV_START, 31) && (
+			ft_memcmp(&((uint8_t *)pem->content)[(pem->content_size - 29
+			- get_end_offset(pem->content, pem->content_size))],
+					RSA_PRIV_END, 29))))
+	{
+		return (cut_start_end(&pem, 60, 31));
+	}
+	else if ((ft_memcmp(pem->content, RSA_PUB_START, 26) && (
+			ft_memcmp(&((uint8_t *)pem->content)[(pem->content_size - 24
+			- get_end_offset(pem->content, pem->content_size))],
+					RSA_PUB_END, 24))))
+	{
+		return (cut_start_end(&pem, 50, 26));
+	}
+	return (NULL);
 }
---
+++ srcs/rsa_utils/rsa_key_converter.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rsa_key_converter.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gemerald <gemerald@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/21 18:55:35 by gemerald          #+#    #+#             */
+/*   Updated: 2021/03/21 20:00:17 by gemerald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ssl.h"
+#include "rsa.h"
+
+uint64_t  get_field(uint8_t *mem, size_t size)
+{
+	uint64_t result;
+	uint8_t part[8];
+	size_t border;
+	size_t i;
+	size_t j;
+
+	result = 0;
+	ft_bzero(part, 8);
+	if (mem[0] != 2)
+		return 0;
+	border = mem[1];
+	i = 2;
+	j = 0;
+	while (i < (2 + border) && i < size)
+	{
+		part[j] = mem[i];
+		i++;
+		j++;
+	}
+	result = *(uint64_t *)part;
+	return (result);
+}
+
+void 	fill_rsa_priv_key(uint64_t nums[], t_rsa_key *key)
+{
+	key->version = nums[0];
+	key->modulus = nums[1];
+	key->public_exponent = nums[2];
+	key->private_exponent = nums[3];
+	key->prime1 = nums[4];
+	key->prime2 = nums[5];
+	key->exponent1 = nums[6];
+	key->exponent2 = nums[7];
+	key->coefficient = nums[8];
+}
+
+void 	fill_rsa_pub_key(uint64_t nums[], t_rsa_key *key)
+{
+	key->modulus = nums[0];
+	key->public_exponent = nums[1];
+}
+
+t_rsa_key convert_der_to_priv_key(t_list *der)
+{
+	t_rsa_key key;
+	uint8_t *mem;
+	int i;
+	size_t cur_ind;
+	uint64_t nums[9];
+
+	ft_bzero(&key, sizeof(t_rsa_key));
+	ft_bzero(nums, sizeof(uint64_t) * 9);
+	mem = (uint8_t *)der->content;
+	if (mem[0] != 30 || mem[1] != der->content_size - 2)
+		return (key);
+	i = -1;
+	cur_ind = 2;
+	while (++i < 9 && cur_ind < der->content_size)
+	{
+		nums[i] = get_field(&mem[cur_ind], der->content_size);
+		cur_ind += mem[cur_ind  + 1];
+	}
+	if (i == 2)
+		fill_rsa_pub_key(nums, &key);
+	else
+		fill_rsa_priv_key(nums, &key);
+	return (key);
+}
